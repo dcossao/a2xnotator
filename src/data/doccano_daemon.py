0e6ae55979cbab4dce47134fcb8a42b93a79dc39
HY72RN <tomas.sostak@ing.com>
2019-10-15 15:06:29 +0200

    Add sqlite_2_pandas

--- src/data/doccano_daemon.py
+++ src/data/doccano_daemon.py
@@ -1,22 +1,47 @@
 import sqlite3
-import src as root
-from pathlib import Path
-import os
+import pandas as pd
 
 NUMBER_VOTES = 3
 BATCH_SIZE = 10
 MINIMUM_SAMPLES = 100
 
-path = Path(root.__file__)
-path = os.path.join(Path(root.__file__).parents[2], 'a2xnotator_ui/doccano/app/db.sqlite3')
 
-conn = sqlite3.connect(path)
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by the db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Exception as e:
+        print(e)
 
-def create_batch():
-    pass
+    return conn
+
+
+def sqlite_2_pandas(conn, table):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM `{}`".format(table))
+    return pd.DataFrame(cur.fetchall(), columns=[desc[0] for desc in cur.description])
+
+
+def create_batch(conn):
+    cur = conn.cursor()
+    cur.execute("SELECT name FROM api_project")
+    return list(cur.fetchall())
+
 
 def assign_project():
-   pass
+    pass
+
 
 def pull_data():
     pass
+
+
+if __name__ == "__main__":
+    conn = create_connection('db.sqlite3')
+    results = sqlite_2_pandas(conn, "api_project")
+    print(results)
